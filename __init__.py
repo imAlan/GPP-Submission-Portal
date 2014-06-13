4e13f60d8ccf710af171e1f3132a1db6d2bb4cba
Alan Chen <alanchen@nyu.edu>
2014-06-13 16:55:35 -0400

    removed a a few config files

--- __init__.py
+++ __init__.py
@@ -2,5 +2,4 @@ from flask import Flask
 app = Flask(__name__, instance_relative_config=True)  # activate the instance folder
 app.config.from_object('config.default')  # access app.config.default["var name"] and loads default config
 app.config.from_pyfile('config.py')  # loads the instance/config.py file
-app.config.from_envvar('APP_CONFIG_FILE')  # loads the variable specified from env var set in run.py
 
