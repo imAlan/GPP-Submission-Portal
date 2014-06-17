3aab1b903afe7029214616b3aca552be1d08bb0f
Alan Chen <alanchen@nyu.edu>
2014-06-17 11:15:19 -0400

    Added navbar and used flask bootstrap in template and views

--- gpp submission protal.py
+++ gpp submission protal.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
