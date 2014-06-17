b3be10d5f3b203bb61916fa66d3f4db36ef59f37
Alan Chen <alanchen@nyu.edu>
2014-06-17 11:43:51 -0400

    added logo

--- gpp submission protal.py
+++ gpp submission protal.py
@@ -5,10 +5,6 @@ app = Flask(__name__)
 Bootstrap(app)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-@app.route('/home')
 def home():
     return render_template('index.html')
 
