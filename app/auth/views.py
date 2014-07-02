a9249d07c602f34e5dec0463d7cac33dcf115748
Alan Chen <alanchen@nyu.edu>
2014-07-02 12:02:59 -0400

    created logout route

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,7 +2,7 @@ from flask import render_template, flash, redirect, url_for, request
 from . import auth
 from .forms import LogInForm
 from ..models import User
-from flask.ext.login import login_user
+from flask.ext.login import login_user, login_required, logout_user
 
 @auth.route('/')
 @auth.route('/index', methods=['POST', 'GET'])
@@ -16,3 +16,9 @@ def index():
             login_user(user, form.remember_me.data)
             return redirect(url_for('home'))
     return render_template('auth/index.html', form=form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.index'))
