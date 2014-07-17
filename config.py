6aad1a1a25bcb5d6757b611073b7594efcb81297
Alan Chen <alanchen@nyu.edu>
2014-07-17 11:19:34 -0400

    removed venv and config

--- config.py
+++
@@ -1,7 +0,0 @@
-from app import app
-#Debug and sqlalchemy_echo overvides the config.py variables
-#Remove variables when in production
-#DEBUG = True
-#SQLALCHEMY_ECHO = True  #logs sql
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:password@localhost/submission"
-app.config['SECRET_KEY'] = 'Key To Be Determine'  # secret key to prevent csrf
