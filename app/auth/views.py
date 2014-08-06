1bf28fd028d50ae3f09f7da86cf5c2255e8f33e0
Alan Chen <alanchen@nyu.edu>
2014-08-06 16:01:55 -0400

    Tracks User Last LoginTracks User Last Login

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,9 @@
 from flask import render_template, redirect, url_for, request
 from . import auth
 from .forms import LogInForm, ForgotPasswordForm, AccountForm
-from ..models import User
+from ..models import User, db
 from flask.ext.login import login_user, login_required, logout_user, current_user
+import datetime
 
 @auth.route('/', methods=['POST', 'GET'])
 def index():
@@ -16,6 +17,8 @@ def index():
         password = form.password.data
         if user is not None and user.verify_password(password):
             login_user(user, remember=form.remember_me.data)
+            user.last_visited = datetime.date.today()
+            db.session.commit()
             return redirect(url_for('home'))
     return render_template('auth/index.html', form=form, PassForm=PassForm, AccForm=AccForm)
 
